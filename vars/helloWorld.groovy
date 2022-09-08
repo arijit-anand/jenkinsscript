78d5d21b1f0e0870a67b88b043963ab7d35f41ab
arijit-anand <61275226+arijit-anand@users.noreply.github.com>
2022-09-08 15:53:47 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
-def call(String name, String hobby){
-  echo "Hello World ${name}, oh so you love ${hobby}"
+def call(Map config[:]){
+  echo "Hello World ${config.name}, oh so you love ${config.hobby}"
 }
